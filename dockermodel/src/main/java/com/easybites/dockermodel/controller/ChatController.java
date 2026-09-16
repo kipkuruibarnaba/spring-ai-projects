160fe29197a601321e598f43ec91ff249f302115
BKMUTAI <BKMUTAI@safaricom.co.ke>
2026-09-16 22:37:36 +0300

    Initial commit

---
+++ dockermodel/src/main/java/com/easybites/dockermodel/controller/ChatController.java
@@ -0,0 +1,24 @@
+package com.easybites.dockermodel.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author:BKMUTAI
+ */
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+    private final ChatClient chatClient;
+    public ChatController(ChatClient.Builder chatClintBuilder){
+        this.chatClient = chatClintBuilder.build();
+    }
+    @GetMapping("/chat")
+    public String chat(@RequestParam("message") String message){
+        System.out.println("Request receive : "+message);
+      return chatClient.prompt(message).call().content();
+    };
+}
